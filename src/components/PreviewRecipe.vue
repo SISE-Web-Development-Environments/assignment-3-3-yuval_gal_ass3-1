eee48460ee8a64fe96a2df586d931a77cf427a42
Gal <galrosenthal@gmail.com>
2020-06-13 22:44:25 +0300

    Home Page Preview is working fine :)

---
+++ src/components/PreviewRecipe.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="Preview" v-show="recipe.id" >
+    <div id="recipeAndBtns">
+      <a @click="lastRecipe" class="previous round arrows-btns">&#8249;</a>
+      <div id="recipe">
+        <h2>{{ recipe.title }}</h2>
+        <img class="column" :src="recipe.image_url"/>
+        <div class="column img-text-container">
+          <p>prepTime: {{ recipe.prepTime }}</p>
+          <p>popularity: {{ recipe.popularity }}</p>
+          <p>isVegan: {{ recipe.vegan }}</p>
+          <p>isVegi: {{ recipe.vegetarian }}</p>
+          <p>isGlutenFree: {{ recipe.glutenFree }}</p>
+          <p>url: <a v-bind:href="recipe.url">link to the recipe</a></p>
+        </div>
+      </div>
+      <a @click="nextRecipe" class="next round arrows-btns">&#8250;</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PreviewRecipe',
+  props: {
+    recipe: Object,
+    nextRecipe: Function,
+    lastRecipe: Function
+  }
+}
+</script>
+
+<style scoped>
+.Preview {
+  justify-content: center;
+  content: "";
+  display: inline-block;
+  clear: both;
+}
+img {
+  max-width: 30%;
+  height: auto;
+  background-color: white;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  margin-bottom: 25px;
+}
+.img-text-container {
+  text-align: left;
+  padding: 10px 20px;
+}
+.column {
+  float: left;
+  width: 50%;
+}
+
+#recipeAndBtns{
+  position: relative;
+}
+#recipeAndBtns .column{
+  display: inline-block;
+}
+
+.arrows-btns {
+  text-decoration: none;
+  display: inline-block;
+  padding: 8px 16px;
+  position: absolute;
+  top: 50%;
+  transform: translateY(300%);
+  /*width: 38px;*/
+  /*height: 38px;*/
+}
+
+.arrows-btns:hover {
+  background-color: #979696;
+  color: black;
+}
+
+.previous {
+  background-color: #f1f1f1;
+  color: black;
+  left: 0;
+  top: 50%;
+  transform: translateX(-150%) translateY(300%);
+}
+
+.next {
+  background-color: #f1f1f1;
+  color: black;
+  right: 0;
+}
+
+.round {
+  border-radius: 50%;
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="home-main-view">
+    <div class="column left-side">
+      <PreviewRecipe :recipe="getRandomRecipes[currentIndex]" :lastRecipe="back" :nextRecipe="next" :key="currentIndex"/>
+    </div>
+    <div class="column right-side">
+      <h1>Right</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import PreviewRecipe from '@/components/PreviewRecipe'
+import axios from 'axios'
+
+async function getRecipesData () {
+  let randomIds
+  await axios
+    .get('http://localhost/recipes/get_random_recipe_id?numberToRetrieve=3')
+    .then(response => (randomIds = response.data))
+    .then(response => console.log('The random IDs from axios: ' + response))
+  let recipeId
+  const recipesArray = []
+  for (recipeId in randomIds) {
+    await fetch('http://localhost/recipes/preview/recId/' + randomIds[recipeId])
+      .then(response => {
+        console.log(response)
+        return response.json()
+      })
+      .then((jsonData) => {
+        console.log(jsonData)
+        recipesArray.push(jsonData)
+      })
+  }
+  return recipesArray
+}
+
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      currentIndex: 0
+    }
+  },
+  // computed: {
+  //   getIds () {
+  //     return this.getRandomIds()
+  //   }
+  // },
+  asyncComputed: {
+    async getRandomRecipes () {
+      // return [1, 2, 3]
+      return await getRecipesData()
+    }
+  },
+  methods: {
+    next () {
+      if (this.currentIndex + 1 <= this.getRandomRecipes.length - 1) {
+        this.currentIndex++
+      } else {
+        this.currentIndex = 0
+      }
+    },
+    back () {
+      if (this.currentIndex - 1 >= 0) {
+        this.currentIndex--
+      } else {
+        this.currentIndex = this.getRandomRecipes.length - 1
+      }
+    }
+  },
+  components: {
+    PreviewRecipe
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+  .column {
+    float: left;
+    width: 50%;
+  }
+
+  /* Clear floats after the columns */
+  .home-main-view:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+  .left-side {
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+  }
+  .right-side{
+    text-align: right;
+  }
+</style>
